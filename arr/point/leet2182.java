d9eb65a4ec4bfa0245ab647ec5f21bec0672c86b
wztsir <1541529408@qq.com>
2024-01-13 11:22:53 +0800

    imitate

---
+++ arr/point/leet2182.java
@@ -0,0 +1,44 @@
+package 字符串;
+
+import java.util.Collections;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+/**
+ * @ClassName: leet2182
+ * @Description:
+ * @Author: wzt
+ * @Create: 2024-01-13 10:20
+ **/
+
+public class leet2182 {
+    public String repeatLimitedString(String s, int repeatLimit) {
+        int[] hash = new int[26];
+        for (char c : s.toCharArray()) {
+            hash[c-'a']++;
+        }
+        StringBuilder str = new StringBuilder();
+        // i指向最大的， j指向次大的
+        // 还有一个参数 repeat// 重点是i,j 什么时候移动
+        int m = 0;
+        for(int i = 25, j = 24; i >=0 && j >= 0;){
+            if(hash[i] == 0){
+                i--;
+                m = 0;
+            }
+            else if(m < repeatLimit){
+                str.append((char)('a' + i));
+                hash[i]--;
+                m++;
+            }else if(hash[j] == 0 || i <= j){//考虑j的移动, 遗漏一个关键 i>j
+                j--;
+            }else if(m == repeatLimit){
+                str.append((char)('a' + j));
+                hash[j]--;
+                m = 0;
+            }
+        }
+
+        return str.toString();
+    }
+}
--- arr/window/leet995.java
+++ arr/window/leet995.java
@@ -1,5 +1,28 @@
-class Solution {
-    public int minKBitFlips(int[] nums, int k) {
 
+
+import java.util.*;
+
+/**
+ * @ClassName: leet995
+ * @Description:
+ * @Author: wzt
+ * @Create: 2024-01-12 11:56
+ **/
+
+public class leet995 {
+    public int minKBitFlips(int[] nums, int k) {
+        int res = 0;
+        Queue<Integer> q = new LinkedList<>();
+        //队列的写法细节，本质是模拟，但是要模拟的好，可以先出队，后入队，同时注意if不是并列关系
+        for(int i = 0; i < nums.length; i++){
+            if(!q.isEmpty() && q.peek() + k <= i)q.remove();
+
+            if(q.size() % 2 == nums[i]){
+                q.add(i);
+                res++;
+                if(i + k > nums.length)return -1;
+            }
+        }
+        return res;
     }
 }
